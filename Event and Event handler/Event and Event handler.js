8eef2c8d6569a2f9fe42f9990bb8ea7967915555
hyebun <hyebun96@naver.com>
2021-04-10 15:27:43 +0900

    Event and Event handler

--- Event and Event handler/Event and Event handler.js
+++ Event and Event handler/Event and Event handler.js
@@ -2,7 +2,7 @@
 
 const title = document.querySelector("#title");
 
-function hadnlerResize(){
+function handleResize(){
     console.log("I have been resized.");
 }
 
@@ -14,12 +14,28 @@ function handleResize(event){
 }
 
 // event 발생할 때마다, 어떤 event에 반응했는지 console에 출력
-window.addEventListener("resize", handleResize());
+window.addEventListener("resize", handleResize);
 
 function handleClick(){
     title.style.color = "white";
+    title.innerHTML = "Just do it.";
 }
 
 title.addEventListener("click", handleClick);
 
 
+//================================================================
+const buttonElement = document.getElementById('btn');
+
+// 콜백 함수를 제공함으로서 '클릭' 이벤츠를 처리하는 핸들러 추가
+// 엘리먼트가 클릭될 때마다, '누름!' 팝업이 나타날 것
+buttonElement.addEventListener('click',function(evnet){
+    alert('누름!');
+});
+
+// 호환성을 위해서, 함수가 아닌 'handleEEvent' 속성을 가진 오브젝트도 똑같이 처리
+buttonElement.addEventListener('click',{
+    handleEvent: function(event){
+        alert('handleEvent 함수로 누름!');
+    }
+});
