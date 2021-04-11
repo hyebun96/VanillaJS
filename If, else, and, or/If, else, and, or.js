8c1e96deecb0dce2ef07bb151ee4b05bc60b75f9
hyebun <hyebun96@naver.com>
2021-04-11 19:21:13 +0900

    If.else, and, or

---
+++ If, else, and, or/If, else, and, or.js
@@ -0,0 +1,49 @@
+if(10 > 5){
+    console.log("hi");
+} else {
+    console.log("ho");
+}
+
+if(10 == 5){
+    console.log("hi");
+} else {
+    console.log("ho");
+}
+
+if(true){
+    console.log("hi");
+} else {
+    console.log("ho");
+}
+
+if("hyebun" == "hyebun"){
+    console.log("hi");
+} else {
+    console.log("ho");
+}
+
+// String == Number
+if("10" == 10){
+    console.log("hi");
+} else if("10" == "10") {
+    console.log("lalala");
+}else{
+    console.log("ho");
+}
+
+// Operation 피연산자
+if(20 > 5 && "hyebun" == "hyebun"){
+    console.log("yes");
+}else{
+    console.log("no");
+}
+
+if(20 > 5 || "hyebun" == "hyebun"){
+    console.log("yes");
+}else{
+    console.log("no");
+}
+
+//=======================================
+
+//Prompt
---
+++ If, else, and, or/If. else, and, or.md
@@ -0,0 +1,21 @@
+# If, else, and, or
+
+### If
+~~~
+if(condition){
+    block
+} else {
+    block
+}
+~~~
+
+### If 조건문 유형
+true && true = true;
+false && true = false;
+true  && false = false;
+false && fasle = fasle;
+
+true || true = true;
+false || true = false;
+true  || false = false;
+false || fasle = fasle;
