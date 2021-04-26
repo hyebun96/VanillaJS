1aa536af42d45c7226b9766f308113b69d3ee132
hyebun <hyebun96@naver.com>
2021-04-26 22:34:16 +0900

    Making  a To Do List

--- 3.4. Making a To Do List part1/Making a To Do List part1.md
+++ 3.4. Making a To Do List part1/Making a To Do List part1.md
@@ -1,2 +1,26 @@
 # Making a To Do List part1
 
+### Node.appendChild()
++ 메소드는 한 노드를 특정 부모 노드의 자식 노드 리스트 중 마지막 자식으로 붙임
++ 만약 주어진 노드가 이미 문서에 존재하는 노드를 참조하고 있다면 appendChild()메소드는 노드를 현재 위치에서 새로운 위치로 이동시킴
+
+~~~
+var aChild = element.appendChild(aChild);
+~~~
+<br/>
+
+### 예시
+~~~
+// 새로운 단락 요소를 생성하고 문서에 있는 바디 요소의 끝에 붙입니다.
+var p = document.createElement("p");
+document.body.appendChild(p);
+~~~
+<br/>
+
+### Document.createElement()
++ HTML문서에서 Document.createElement() 메서드는 지정한 tagName의 HTML요소를 만들어 반환
+~~~
+let element = document.createElement(tagName[, options]);
+~~~
++ tagName : 생성할 요소의 유형을 가리키는 문자열
++ option : is 속성 하나를 가진 ElementCreationOptions객체 속성의 값은 customElements.define()을 사용해 정의한 사용자 정의 요소
--- 3.4. Making a To Do List part1/todo.js
+++ 3.4. Making a To Do List part1/todo.js
@@ -1,17 +1,25 @@
 const toDoForm = document.querySelector(".js-toDoForm"),
-    toDoInput = form.querySelector("input"),
+    toDoInput = toDoForm.querySelector("input"),
     toDoList = document.querySelector(".js-toDoList");
 
 const TODOS_LS = "toDos";
 
 function paintToDo(text){
-    console.log(text);
+    const li = document.createElement("li");
+    const delBtn = document.createElement("button");
+    delBtn.innerText = "❌";
+    const span = document.createElement("span");
+    span.innerText = text;
+    li.appendChild(span);
+    li.appendChild(delBtn);
+    toDoList.appendChild(li);
 }
 
 function handleSubmit(event){
     event.preventDefault();
     const currentValue = toDoInput.value;
     paintToDo(currentValue);
+    toDoInput.value="";
 }
 
 function loadToDos(){
